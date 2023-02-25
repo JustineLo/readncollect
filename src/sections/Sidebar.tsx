755873b1627a5800f73eec9ddf418234867823a6
Justine Lo <juustine.lo@gmail.com>
2023-02-25 12:00:09 +0100

    Sidebar

--- src/sections/Sidebar.tsx
+++ src/sections/Sidebar.tsx
@@ -2,7 +2,9 @@ import styled from "styled-components";
 import Button from "../components/Button";
 import Icon from "../components/Icon";
 import { logout } from "../firebase";
-import { MdAccountCircle } from "react-icons/md";
+import { MdAccountCircle, MdLogout } from "react-icons/md";
+import { RiScissors2Fill } from "react-icons/ri";
+import { CgCollage } from "react-icons/cg";
 import { Tooltip } from "react-tooltip";
 import AppState from "../state/AppState";
 import { useContainer } from "unstated-next";
@@ -15,7 +17,16 @@ const SidebarContainer = styled.section`
   background: var(--purple-medium-transparent);
   display: flex;
   flex-direction: column;
-  gap: 1rem;
+  justify-content: space-between;
+  gap: 5rem;
+  padding: 5rem 0;
+  box-sizing: border-box;
+`;
+
+const PageIcons = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5rem;
 `;
 
 const Sidebar = ({}: SidebarProps) => {
@@ -23,15 +34,29 @@ const Sidebar = ({}: SidebarProps) => {
   return (
     <>
       <SidebarContainer>
-        <Button onClick={logout}>Logout</Button>
         <Icon
           tooltipContent={`Connected as ${user?.email}`}
-          tooltipId="user-tooltip"
+          tooltipId="sidebar-tooltip"
         >
           <MdAccountCircle size="2rem" />
         </Icon>
+        <PageIcons>
+          <Icon tooltipContent="Articles" tooltipId="sidebar-tooltip">
+            <RiScissors2Fill size="2rem" />
+          </Icon>
+          <Icon tooltipContent="Collage Builder" tooltipId="sidebar-tooltip">
+            <CgCollage size="2rem" />
+          </Icon>
+        </PageIcons>
+        <Icon
+          onClick={logout}
+          tooltipContent="Logout"
+          tooltipId="sidebar-tooltip"
+        >
+          <MdLogout size="1.5rem" />
+        </Icon>
       </SidebarContainer>
-      <Tooltip id="user-tooltip" />
+      <Tooltip id="sidebar-tooltip" />
     </>
   );
 };
