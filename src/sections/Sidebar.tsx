5f8256a39fea1623a3a923f4ede491128fcbc62c
Justine Lo <juustine.lo@gmail.com>
2023-02-25 12:43:24 +0100

    add dashboard icon

--- src/sections/Sidebar.tsx
+++ src/sections/Sidebar.tsx
@@ -1,5 +1,5 @@
-import { CgCollage } from "react-icons/cg";
-import { MdAccountCircle, MdLogout } from "react-icons/md";
+import { FaRegClipboard } from "react-icons/fa";
+import { MdAccountCircle, MdLogout, MdSpaceDashboard } from "react-icons/md";
 import { RiScissors2Fill } from "react-icons/ri";
 import { Tooltip } from "react-tooltip";
 import styled from "styled-components";
@@ -40,11 +40,14 @@ const Sidebar = ({}: SidebarProps) => {
           <MdAccountCircle size="2rem" />
         </Icon>
         <PageIcons>
+          <Icon tooltipContent="Dashboard" tooltipId="sidebar-tooltip">
+            <MdSpaceDashboard size="2rem" />
+          </Icon>
           <Icon tooltipContent="Articles" tooltipId="sidebar-tooltip">
             <RiScissors2Fill size="2rem" />
           </Icon>
           <Icon tooltipContent="Collage Builder" tooltipId="sidebar-tooltip">
-            <CgCollage size="2rem" />
+            <FaRegClipboard size="2rem" />
           </Icon>
         </PageIcons>
         <Icon
