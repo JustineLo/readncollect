bc5c87a98113585f7997a8807f9dcfd02080379b
Justine Lo <justine@ecotree.fr>
2023-04-01 17:59:13 +0200

    fix handling images

--- src/utils/articleUtils.ts
+++ src/utils/articleUtils.ts
@@ -1,21 +1,21 @@
+import img0 from "@/assets/defaultimages/0.jpg";
+import img1 from "@/assets/defaultimages/1.jpg";
+import img10 from "@/assets/defaultimages/10.jpg";
+import img11 from "@/assets/defaultimages/11.jpg";
+import img2 from "@/assets/defaultimages/2.jpg";
+import img3 from "@/assets/defaultimages/3.jpg";
+import img4 from "@/assets/defaultimages/4.jpg";
+import img5 from "@/assets/defaultimages/5.jpg";
+import img6 from "@/assets/defaultimages/6.jpg";
+import img7 from "@/assets/defaultimages/7.jpg";
+import img8 from "@/assets/defaultimages/8.jpg";
+import img9 from "@/assets/defaultimages/9.jpg";
 import { doc, updateDoc } from "firebase/firestore";
-import img0 from "../assets/defaultimages/0.jpg";
-import img1 from "../assets/defaultimages/1.jpg";
-import img10 from "../assets/defaultimages/10.jpg";
-import img11 from "../assets/defaultimages/11.jpg";
-import img2 from "../assets/defaultimages/2.jpg";
-import img3 from "../assets/defaultimages/3.jpg";
-import img4 from "../assets/defaultimages/4.jpg";
-import img5 from "../assets/defaultimages/5.jpg";
-import img6 from "../assets/defaultimages/6.jpg";
-import img7 from "../assets/defaultimages/7.jpg";
-import img8 from "../assets/defaultimages/8.jpg";
-import img9 from "../assets/defaultimages/9.jpg";
 
+import { saveAs } from "file-saver";
 import { db } from "../firebase";
 import { Article, Highlight } from "../types/Article";
 import { User } from "../types/User";
-import { saveAs } from "file-saver";
 
 export function getUpdatedArticles(
   user: User,
